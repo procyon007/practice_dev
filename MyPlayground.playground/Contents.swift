3da5961f1d60a418712d4cad8dd7fae56535d34c
procyon007 <ta.uta07@gmail.com>
2016-08-28 15:17:22 +0900

    Git新環境のテスト

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Arrays
+
+var array = [17, 25, 13, 47]
+
+print(array[2])
+
+print(array.count)
+
+array.append(56)
+
+array.removeAtIndex(3)
+
+print(array)
+
+array.sort()
